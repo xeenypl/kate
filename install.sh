2ffc27a962a32030dfafa72534f3b2bae962e0d8
xeeny <xeeny@wp.pl>
2022-01-15 19:14:36 +0100

    Updaded install script for no confermtion is needed.

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # Libs for dwm.
-sudo pacman -S libxft libxinerama
+sudo pacman --noconfirm -S libxft libxinerama
 # Xorg.
-sudo pacman -S xorg-server xorg-xinit xorg-xsetroot
-sudo pacman -S xcape xclip xsel scrot alacritty
+sudo pacman --noconfirm -S xorg-server xorg-xinit xorg-xsetroot
+sudo pacman --noconfirm -S xcape xclip xsel scrot alacritty
 # Fonts.
-sudo pacman -S ttf-dejavu ttf-nerd-fonts-symbols-mono
+sudo pacman --noconfirm -S ttf-dejavu ttf-nerd-fonts-symbols-mono
 # Multimedia.
-sudo pacman -S qutebrowser nnn mpv sxiv xwallpaper imagemagick
+sudo pacman --noconfirm -S qutebrowser nnn mpv sxiv xwallpaper imagemagick
 # misc tools 
-sudo pacman -S ripgrep fzf exa aria2 fd bat zip unzip unrar
+sudo pacman --noconfirm -S ripgrep fzf exa aria2 fd bat zip unzip unrar
 
-sudo pacman -S python-pip
+sudo pacman --noconfirm -S python-pip
