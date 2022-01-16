ac0852a9c50639e604f912d5bb98d6971783a293
xeeny <xeeny@wp.pl>
2022-01-16 20:37:20 +0100

    Added a dunst to run to sturt up.

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ sudo pacman --noconfirm -S libxft libxinerama
 sudo pacman --noconfirm -S xorg-server xorg-xinit xorg-xsetroot
 sudo pacman --noconfirm -S xcape xclip xsel scrot alacritty
 # Fonts.
-sudo pacman --noconfirm -S ttf-dejavu ttf-nerd-fonts-symbols-mono jq
+sudo pacman --noconfirm -S ttf-dejavu ttf-nerd-fonts-symbols-mono
 # Multimedia.
 sudo pacman --noconfirm -S qutebrowser nnn mpv sxiv xwallpaper imagemagick
 # misc tools 
-sudo pacman --noconfirm -S ripgrep fzf exa aria2 fd bat zip unzip unrar
+sudo pacman --noconfirm -S ripgrep fzf exa aria2 fd bat zip unzip unrar jq
 
 sudo pacman --noconfirm -S dunst libnotify
 
