7e1ec744601ee4f3758a049999310d68508fc82c
xeeny <xeeny@wp.pl>
2022-02-20 00:21:04 +0100

    Update install.sh convert it to fish and move stuff from install.md.

--- install.sh
+++ install.sh
@@ -1,19 +1,31 @@
-#!/bin/sh
+#!/usr/bin/fish
+
+pushd /tmp/
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+popd
+
 # Libs for dwm.
-sudo pacman --noconfirm -S libxft libxinerama
+yay --noconfirm -S libxft libxinerama
 # Xorg.
-sudo pacman --noconfirm -S xorg-server xorg-xinit xorg-xsetroot
-sudo pacman --noconfirm -S xcape xclip xsel scrot alacritty
+yay --noconfirm -S xorg-server xorg-xinit xorg-xsetroot
+yay --noconfirm -S xcape xclip xsel scrot alacritty
+yay --noconfirm -S dunst libnotify picom
 # Fonts.
 sudo pacman --noconfirm -S ttf-dejavu ttf-nerd-fonts-symbols-mono
-# Multimedia.
-sudo pacman --noconfirm -S qutebrowser nnn mpv sxiv xwallpaper imagemagick
-# misc tools 
+# Audio
+yay --noconfirm -S alsa-utils pulseaudio pulseaudio-alsa pulsemixer
+# Misc tools 
 sudo pacman --noconfirm -S ripgrep fzf exa aria2 fd bat zip unzip unrar jq man man-pages
+# Pdf reader
+yay --noconfirm -S zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps
+# Multimedia.
+yay --noconfirm -S qutebrowser nnn mpv sxiv xwallpaper imagemagick
+yay --noconfirm -S brave-bin neovim-nightly-bin
+yay --noconfirm -S gimp krita inkscape blender
 
-sudo pacman --noconfirm -S dunst libnotify picom
-
-sudo pacman --noconfirm -S alsa-utils pulseaudio pulseaudio-alsa pulsemixer
-
-sudo pacman --noconfirm -S zathura zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps
-
+curl -sL https://git.io/fisher | source
+fisher install jorgebucaran/fisher
+fisher install jorgebucaran/hydro
+fisher install jorgebucaran/autopair.fish
